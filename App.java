aa35007b6e02d79be940191fc3096307b02ab27a
SNIP001 <73440452+SNIP001@users.noreply.github.com>
2021-05-03 18:55:54 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+package $15_interface;
+
+
+public class App {
+    public static void main(String[] args) {
+        Honda h = new Honda();
+        h.go();
+        h.stop();
+    }
+}
---
+++ Car.java
@@ -0,0 +1,7 @@
+package $15_interface;
+
+
+public interface Car {
+    void go();  // default is public abstract
+    void stop();
+}
